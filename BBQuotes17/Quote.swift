a50bb4069030b171d22dd3329b164ef1d6f0929c
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2024-11-27 18:54:37 +0800

    Feature/add initial models (#1)
    * Add initial models for breaking bad app
    
    * Add field to character model

---
+++ BBQuotes17/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  BBQuotes17
+//
+//  Created by Lyle Dane Carcedo on 11/27/24.
+//
+
+import Foundation
+
+struct Quote: Decodable {
+    let quote: String
+    let character: String
+}
