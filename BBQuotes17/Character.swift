5d4c90328ca044090ea333b9d4efcbb6cd4da5d3
lyleverythinG <75658617+lyleverythinG@users.noreply.github.com>
2024-11-27 18:37:16 +0800

    Add initial models for breaking bad app

---
+++ BBQuotes17/Character.swift
@@ -0,0 +1,19 @@
+//
+//  Character.swift
+//  BBQuotes17
+//
+//  Created by Lyle Dane Carcedo on 11/27/24.
+//
+
+import Foundation
+
+struct Character: Decodable {
+    let name: String
+    let birthday: String
+    let occupations: [String]
+    let images: [URL]
+    let aliases: [String]
+    let status: String
+    let portrayedBy: String
+}
+                        
---
+++ BBQuotes17/Death.swift
@@ -0,0 +1,15 @@
+//
+//  Death.swift
+//  BBQuotes17
+//
+//  Created by Lyle Dane Carcedo on 11/27/24.
+//
+
+import Foundation
+
+struct Death: Decodable {
+    let character: String
+    let image: URL
+    let details: String
+    let lastWords: String
+}
